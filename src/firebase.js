921d8567c3b50bb548f606bded63cafa18f1fd32
Aayush Jain <jaayush014@gmail.com>
2021-01-11 00:46:18 +0530

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBgp92diYbRTCdfA0oloPehkuJU4n3MB9c',
+  authDomain: 'amazo-nclone7.firebaseapp.com',
+  projectId: 'amazo-nclone7',
+  storageBucket: 'amazo-nclone7.appspot.com',
+  messagingSenderId: '1045071738610',
+  appId: '1:1045071738610:web:c604ceb7b550b7f6f4026b',
+  measurementId: 'G-9HSFC9BBKP',
+};
